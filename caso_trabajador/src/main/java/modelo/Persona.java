05fefad121daa86b7ee1857eaf51434eb06a2df2
mjosepc <mjose.powell@gmail.com>
2023-11-23 13:05:35 -0300

    2 commit paquete modelo

--- caso_trabajador/src/main/java/modelo/Persona.java
+++ caso_trabajador/src/main/java/modelo/Persona.java
@@ -1,13 +1,16 @@
 package modelo;
 
 public class Persona {
-
 	protected String nombre;
 	protected String apellido;
 	protected String rut;
 
-	public Persona() {
-		// TODO - implement Persona.Persona
+	public Persona(String nombre, String apellido, String rut) {
+
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.rut = rut;
+
 		throw new UnsupportedOperationException();
 	}
 
--- caso_trabajador/src/main/java/modelo/Trabajador.java
+++ caso_trabajador/src/main/java/modelo/Trabajador.java
@@ -5,14 +5,16 @@ public class Trabajador extends Persona {
 	private String isapre;
 	private String afp;
 
+	public Trabajador(String nombre, String apellido, String rut, String isapre, String afp){
+		super(nombre, apellido, rut);
+		this.isapre = isapre;
+		this.afp = afp;
+		throw new UnsupportedOperationException();
+	}
 	public String getIsapre() {
 		return this.isapre;
 	}
 
-	/**
-	 * 
-	 * @param isapre
-	 */
 	public void setIsapre(String isapre) {
 		this.isapre = isapre;
 	}
@@ -20,18 +22,7 @@ public class Trabajador extends Persona {
 	public String getAfp() {
 		return this.afp;
 	}
-
-	/**
-	 * 
-	 * @param afp
-	 */
 	public void setAfp(String afp) {
 		this.afp = afp;
 	}
-
-	public Trabajador() {
-		// TODO - implement Trabajador.Trabajador
-		throw new UnsupportedOperationException();
-	}
-
 }
